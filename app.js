600734e5968874a0ecda0ce9522ba3ff3e3ce774
dnyaneshwaripandhare062@gmail.com <your@email.com>
2024-01-06 09:14:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,11 +33,11 @@ app.use("/designer/", designerRouter);
 app.use("/work/", sampleRouter, BeforeAfter);
 
 // //static file
-// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
